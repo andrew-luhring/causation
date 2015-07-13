2682ad434bd93ac9a5489d713220faa742fae3a7
andrew-luhring <andrew.luhring@gmail.com>
2015-07-13 00:43:47 -0500

    indent states

--- public/js/script.js
+++ public/js/script.js
@@ -6,48 +6,48 @@ angular.module('dft', ['ngSanitize', 'ui.router'])
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
-  $stateProvider.state('loggedIn', {
-    url: '/'
-  , controller: 'GlobalController'
-  , controllerAs: 'global'
-  })
-  .state('home', {
-    templateUrl: '/views/partials/home.html'
-  , url: 'home'
-  })
-  .state('map', {
-    template: '<roo-map></roo-map>'
-  , url: '/map'
-  })
-  .state('about', {
-    templateUrl: '/views/layouts/about.html'
-  , url: '/about'
-  })
-  .state('data', {
-    templateUrl: '/views/layouts/data.html'
-  , url: '/data'
-  })
-  .state('login', {
-    templateUrl: '/views/partials/login.html'
-  , url: '/login'
-  })
-  .state('contact', {
-    url: '/contact'
-  , templateUrl: '/views/partials/contact.hml'
-  })
-  .state('contribute', {
-    templateUrl: '/views/partials/contribute.html'
-  , url: '/contribute'
-  })
-  .state('contribute.money', {
-    url: '/money'
-  })
-  .state('contribute.info', {
-    url: '/info'
-  })
-  .state('contribute.code', {
-    url: '/code'
-  });
+    $stateProvider.state('loggedIn', {
+      url: '/'
+    , controller: 'GlobalController'
+    , controllerAs: 'global'
+    })
+    .state('home', {
+      templateUrl: '/views/partials/home.html'
+    , url: 'home'
+    })
+    .state('map', {
+      template: '<roo-map></roo-map>'
+    , url: '/map'
+    })
+    .state('about', {
+      templateUrl: '/views/layouts/about.html'
+    , url: '/about'
+    })
+    .state('data', {
+      templateUrl: '/views/layouts/data.html'
+    , url: '/data'
+    })
+    .state('login', {
+      templateUrl: '/views/partials/login.html'
+    , url: '/login'
+    })
+    .state('contact', {
+      url: '/contact'
+    , templateUrl: '/views/partials/contact.hml'
+    })
+    .state('contribute', {
+      templateUrl: '/views/partials/contribute.html'
+    , url: '/contribute'
+    })
+    .state('contribute.money', {
+      url: '/money'
+    })
+    .state('contribute.info', {
+      url: '/info'
+    })
+    .state('contribute.code', {
+      url: '/code'
+    });
 
     $urlRouterProvider.otherwise('/');
   }]);
