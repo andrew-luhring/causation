bcf4af1fe997035322a219584689ad9c31179b80
andrew-luhring <andrew.luhring@gmail.com>
2016-03-24 11:29:48 -0500

    removes contact info, adds standard-inputs to signup data, moves inline css to less

--- public/js/signup/controllers/signup.ctrl.js
+++ public/js/signup/controllers/signup.ctrl.js
@@ -1,61 +1,29 @@
-angular.module('dft').controller('SignupCtrl', ['$scope', '$http', function($scope, $http) {
-  'use strict';
-  var accessor = this;
-
-  function StandardInput(obj){
-    this.class = obj.class || '';
-    this.label = obj.label || '';
-    this.model = obj.model;
-    this.name = obj.name;
-    this.placeholder = obj.placeholder || obj.label;
-    this.required = obj.required || false;
-    this.type = obj.type || 'text';
-
-    if( !this.model || !this.name){
-      throw new TypeError('model: ' + this.model + ' or name: ' + this.name + ' is null or undefined.');
-    }
-  }
-
-  this.person = {
-    firstname: 'andrew'
-  , lastname: 'luhring'
-  , email: 'andeqoo@gmail.com'
-  , phone: {
-      number: '4196804649'
-    , type: 'cell'
-    , context: 'home'
-  }
-  , address: {
-    street: '365 N Halsted'
-    , unit: '1509'
-    , city: 'Chicago'
-    , postal: '60661'
-  }
-  , notPatient: true
-  , relationship: "parent"
-  , alive: true
-  };
-  this.patient = {
-    firstname: 'Clifton'
-  , lastname: 'Stommel'
-  , phone: {
-      number: '3171231234'
-    , type: 'cell'
-    , context: 'home'
-    }
-  , address: {
-      street: '445 e ohio st'
-    , unit: '3605'
-    , city: 'chicago'
-    , postal: '60611'
-    }
-  };
-  this.names = [{
-    label: 'First name.'
-  , name: 'firstname'
-  , model: 'signupctrl.person.firstname'
-  , required: true
-  }];
+angular.module('dft').controller('SignupCtrl', [
+  '$scope', '$http', 'standardInputConstructor', function($scope, $http, StandardInput) {
+    'use strict';
+    var accessor = this;
+    this.nameInputs = [
+      new StandardInput({
+        label: 'First name.'
+      , name: 'firstname'
+      , model: 'signupctrl.person.firstname'
+      , required: true
+      }),
+      new StandardInput({
+        label: 'Last Name'
+      , name: 'lastname'
+      , model: 'signupctrl.person.lastname'
+      , required: true
+      })
+    ];
+    this.contactInputs = [
+      new StandardInput({
+        label: 'Email'
+      , name: 'email'
+      , model: 'signupctrl.person.email'
+      , required: true
+      })
+    ]
   this.submit = function() {
     var data = {
       person: accessor.person
@@ -65,4 +33,5 @@ angular.module('dft').controller('SignupCtrl', ['$scope', '$http', function($sco
       console.log (dat);
     });
   };
-}]);
+  }
+]);
